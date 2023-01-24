51334b59215427c05e896d4387eac2b7fddc8751
nVietUK <vietuk.nguyen.git@icloud.com>
2023-01-24 22:01:20 +0700

    update

--- .sh/install-python.sh
+++ .sh/install-python.sh
@@ -1,7 +1,7 @@
 apt install -y git build-essential zlibc zlib1g-dev libssl-dev libncurses5-dev libsqlite3-dev libreadline-dev libtk8.6 libgdm-dev libdb4o-cil-dev libpcap-dev make
 git submodule deinit -f cpython
+git submodule update --remote --init cpython
 cd cpython 
-git submodule update --remote cpython/
 
 ./configure
 make -j 100
