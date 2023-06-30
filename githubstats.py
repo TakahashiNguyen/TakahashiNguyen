f3a0e2bc325d94dad89abc8d145fbd367df83b83
nVietUK <vietuk.nguyen.git@icloud.com>
2023-06-30 21:24:03 +0700

    update 2 files and delete 3 files

---
+++ githubstats.py
@@ -0,0 +1,9 @@
+username = 'takahashinguyen'
+
+import requests
+stats = requests.get(f'https://github-readme-stats.vercel.app/api?username={username}&show_icons=true&locale=vi&hide_border=True&theme=swift&hide_title=True&card_width=500')
+toplang = requests.get(f'https://github-readme-stats.vercel.app/api/top-langs?username={username}&layout=compact&langs_count=6&show_icons=true&locale=vi&hide_border=True&theme=swift&card_width=300')
+with open('stats.svg', 'w') as file:
+    file.write(
+        f"""<svg viewBox="0 0 800 175" xmlns="http://www.w3.org/2000/svg" width="800" height="175">\n{stats.text + toplang.text.replace('viewBox="0 0 300 165"', 'viewBox="0 0 300 165" x="500"')}\n</svg>"""
+    )
