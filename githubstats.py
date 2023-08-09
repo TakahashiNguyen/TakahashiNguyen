8e28a077e84e73d8e79b641d1036fa7147894abd
Nguyễn Việt Anh <83152264+TakahashiNguyen@users.noreply.github.com>
2023-08-09 21:30:40 +0700

    Update githubstats.py

--- githubstats.py
+++ githubstats.py
@@ -4,7 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 stats = requests.get(f'https://github-readme-stats.vercel.app/api?username={username}&show_icons=true&locale=vi&hide_border=True&theme=swift&hide_title=True&card_width=400').text
 toplang = requests.get(f'https://github-readme-stats.vercel.app/api/top-langs?username={username}&layout=compact&langs_count=6&show_icons=true&locale=vi&hide_border=True&theme=swift&card_width=300').text.replace('viewBox="0 0 300 165"', 'viewBox="0 0 300 165" x="500"')
-contributestreak = requests.get(f'https://github-readme-streak-stats.herokuapp.com?user={username}&theme=swift&hide_border=true&locale=vi&date_format=j%20M%5B%20Y%5D&card_width=800').text.replace("viewBox=\'0 0 800 195\'",'viewBox="0 0 800 195" y="165"')
+contributestreak = requests.get(f'https://streak-stats.demolab.com?user={username}&theme=swift&hide_border=true&locale=vi&card_width=800').text.replace("viewBox=\'0 0 800 195\'",'viewBox="0 0 800 195" y="165"')
 snake = requests.get(f'https://raw.githubusercontent.com/{username}/{username}/output/github-contribution-grid-snake.svg').text.replace('width="880"', 'width="800" y="360"')
 with open('./dist/stats.svg', 'w') as f:
     f.write(
