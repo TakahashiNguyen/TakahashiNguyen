6f51c26d35213b7824eeca7effb4aed8a0d2be0b
TakahashiNguyen <83152264+TakahashiNguyen@users.noreply.github.com>
2024-04-02 21:16:14 +0700

    update eos.js and eos.html

--- .js/eos.js
+++ .js/eos.js
@@ -6,76 +6,45 @@ firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 var player;
 function getRandomInt(max) {
-	return Math.floor(Math.random() * max);
+  return Math.floor(Math.random() * max);
 }
 function onYouTubeIframeAPIReady() {
-	/*if (getRandomInt(2) === 0)
-	  player = new YT.Player("myVideo", {
-		height: "100%",
-		width: "100%",
-		videoId: "dQw4w9WgXcQ",
-		playerVars: {
-		  playsinline: 1,
-		  autoplay: 1,
-		  loop: 1,
-		  rel: 0,
-		},
-		events: {
-		  onReady: onPlayerReady,
-		  onStateChange: onPlayerStateChange,
-		},
-	  });
-	else
-	  player = new YT.Player("myVideo", {
-		height: "100%",
-		width: "100%",
-		videoId: "kIEWJ1ljEro",
-		playerVars: {
-		  playsinline: 1,
-		  autoplay: 1,
-		  loop: 1,
-		  rel: 0,
-		},
-		events: {
-		  onReady: onPlayerReady,
-		  onStateChange: onPlayerStateChange,
-		},
-	  });*/
-	player = new YT.Player("myVideo", {
-		height: "100%",
-		width: "100%",
-		videoId: "FIqtCVE-U7w",
-		playerVars: {
-			playsinline: 1,
-			autoplay: 1,
-			loop: 1,
-			rel: 0,
-		},
-		events: {
-			onReady: onPlayerReady,
-			onStateChange: onPlayerStateChange,
-		},
-	});
+  ran_value = getRandomInt(3);
+  vid_id = "IlVShjKur0I";
+  if (ran_value === 0) vid_id = "jNxUnXahFcg";
+  else if (ran_value === 1) vid_id = "P7b6NOfJtPY";
+  player = new YT.Player("myVideo", {
+    height: "100%",
+    width: "100%",
+    videoId: vid_id,
+    playerVars: {
+      playsinline: 1,
+      autoplay: 1,
+      loop: 1,
+      rel: 0,
+    },
+    events: {
+      onReady: onPlayerReady,
+      onStateChange: onPlayerStateChange,
+    },
+  });
 }
 
-// 4. The API will call this function when the video player is ready.
 function onPlayerReady(event) {
-	//event.target.playVideo();
-	player.playVideo();
+  player.playVideo();
 }
 
-// 5. The API calls this function when the player's state changes.
 function onPlayerStateChange(event) {
-	console.log(event);
-	if (event.data == YT.PlayerState.PLAYING) {
-		console.log("sus");
-	}
-	if (event.data === -1) {
-		document.getElementById("myVideo").remove();
-		myText.style.filter = "none";
-		subText.style.color = "white";
-	}
+  console.log(event);
+  if (event.data == YT.PlayerState.PLAYING) {
+    console.log("sus");
+  }
+  if (event.data === -1) {
+    document.getElementById("myVideo").remove();
+    myText.style.filter = "none";
+    subText.style.color = "white";
+  }
 }
 function stopVideo() {
-	player.stopVideo();
+  player.stopVideo();
 }
