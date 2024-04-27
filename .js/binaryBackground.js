1dfe6e814c4bc95487f5f8b9e18b471902274396
TakahashiNguyen <83152264+TakahashiNguyen@users.noreply.github.com>
2024-04-28 04:34:09 +0700

    update binaryBackground.html, binaryBackground.js and index.html

--- .js/binaryBackground.js
+++ .js/binaryBackground.js
@@ -4,7 +4,7 @@ const genBinary = (previous = "") => {
     if (previous == "") {
       for (let i = 0; i < stringLength; i++) output += `${getRandomInt(2)}`;
     } else {
-      let numChange = previous.length * (4 / 100);
+      let numChange = previous.length * (9 / 100);
       do {
         numChange -= 1;
         let pos = getRandomInt(previous.length);
@@ -22,32 +22,65 @@ const genBinary = (previous = "") => {
   getFontHeight = () => (getFontSize() * 5) / 10,
   rowNum = () => floor(diagonalLength() / getFontHeight());
 let addRowNum = 0,
-  stringLength = 1000,
+  stringLength = 250,
   randomDigitDelay = 0;
+
+// Generate SVG block
+function genSVGBlock(id, spped) {
+  var svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  svg.setAttribute("class", `leading-[${getFontHeight()}px] block`);
+  svg.setAttribute("id", `svg${id}`);
+  svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+
+  var defs = document.createElementNS("http://www.w3.org/2000/svg", "defs");
+  svg.appendChild(defs);
+
+  var mask = document.createElementNS("http://www.w3.org/2000/svg", "mask");
+  mask.setAttribute("id", `mask${id}`);
+  defs.appendChild(mask);
+
+  var rect = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+  mask.appendChild(rect);
+  rect.setAttribute("class", "fill-white");
+  rect.setAttribute("id", `rect${id}`);
+
+  var text = document.createElementNS("http://www.w3.org/2000/svg", "text");
+  text.setAttribute("x", "50%");
+  text.setAttribute("y", "60%");
+  text.setAttribute("dominant-baseline", "middle");
+  text.setAttribute("text-anchor", "middle");
+  text.setAttribute("id", `text${id}`);
+  text.textContent = genBinary();
+  mask.appendChild(text);
+
+  var rectBlock1 = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+  rectBlock1.setAttribute("class", `dark:fill-white fill-[#370037] animate-[marquee1_${spped}s_linear_infinite] `);
+  rectBlock1.setAttribute("style", `mask: url(#mask${id})`);
+  rectBlock1.setAttribute("id", `rectBlock${id}_1`);
+  svg.appendChild(rectBlock1);
+
+  var rectBlock2 = document.createElementNS("http://www.w3.org/2000/svg", "rect");
+  rectBlock2.setAttribute(
+    "class",
+    `dark:fill-white fill-[#370037]  animate-[marquee2_${spped}s_linear_infinite] absolute`
+  );
+  rectBlock2.setAttribute("style", `mask: url(#mask${id})`);
+  rectBlock2.setAttribute("id", `rectBlock${id}_2`);
+  svg.appendChild(rectBlock2);
+
+  return svg;
+}
 
 // Init binary rows
 function initBinaryRows(from = 0) {
   addRowNum = rowNum();
-  const initSpeed = stringLength / 10 + 500;
+  const initSpeed = stringLength / 10 + 250;
   let prevSpeed = initSpeed;
   for (let i = from; i < addRowNum; i++) {
-    const outterDiv = document.createElement("div"),
-      text1Div = document.createElement("div"),
-      text2Div = document.createElement("div"),
-      text1 = document.createElement("span"),
-      text2 = document.createElement("span"),
-      curSpeed = prevSpeed + (getRandomInt(initSpeed / 20) + 1) * (getRandomInt(2) ? -1 : 1);
-    outterDiv.classList.add("relative", "whitespace-nowrap", "flex", "flex-row");
-    outterDiv.append(text1Div, text2Div);
-    text1Div.append(text1), text2Div.append(text2);
-    text1Div.classList.add(`animate-[marquee1_${curSpeed}s_linear_infinite]`);
-    text2Div.classList.add(`animate-[marquee2_${curSpeed}s_linear_infinite]`, "translate-x-full", "absolute", "top-0");
-    text1.classList.add("inline-block", `leading-[${getFontHeight()}px]`);
-    text2.classList.add("inline-block", `leading-[${getFontHeight()}px]`);
-    (text1.innerText = genBinary()), (text2.innerText = genBinary());
-    (text1.id = `text${i}_1`), (text2.id = `text${i}_2`);
+    const curSpeed = prevSpeed + (getRandomInt(initSpeed / 15) + 1) * (getRandomInt(2) ? -1 : 1),
+      text = genSVGBlock(i, curSpeed);
 
-    ele("binaryDiv").appendChild(outterDiv);
+    ele("binaryDiv").appendChild(text);
     prevSpeed = curSpeed;
   }
 }
@@ -55,8 +88,17 @@ function initBinaryRows(from = 0) {
 // Resize text
 function textResize() {
   for (let i = 0; i < addRowNum; i++) {
-    ele(`text${i}_1`).style.fontSize = getFontSize() + "px";
-    ele(`text${i}_2`).style.fontSize = getFontSize() + "px";
+    ele(`text${i}`).style.fontSize = getFontSize() + "px";
+    ele(`rect${i}`).style.width =
+      ele(`rectBlock${i}_1`).style.width =
+      ele(`rectBlock${i}_2`).style.width =
+      ele(`svg${i}`).style.width =
+        ele(`text${i}`).getComputedTextLength();
+    ele(`rect${i}`).style.height =
+      ele(`rectBlock${i}_1`).style.height =
+      ele(`rectBlock${i}_2`).style.height =
+      ele(`svg${i}`).style.height =
+        getFontSize() * (3 / 4) + "px";
   }
 }
 
@@ -64,8 +106,7 @@ function textResize() {
 async function updateStrings() {
   randomDigitDelay = 1;
   for (let i = 0; i < addRowNum; i++) {
-    ele(`text${i}_1`).innerText = genBinary(ele(`text${i}_1`).innerText);
-    ele(`text${i}_2`).innerText = genBinary(ele(`text${i}_1`).innerText);
+    ele(`text${i}`).textContent = genBinary(ele(`text${i}`).textContent);
   }
   do {
     randomDigitDelay -= 1;
