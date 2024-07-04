98f7fbc7170cad091a7eff39a4d6f4b2a563ead0
TakahashiNguyen <83152264+TakahashiNguyen@users.noreply.github.com>
2024-07-04 15:47:56 +0700

    update WindowsCleaner.ps1 and launch.json

--- .ps1/WindowsCleaner.ps1
+++ .ps1/WindowsCleaner.ps1
@@ -3,16 +3,11 @@ $drives = Get-PSDrive -PSProvider FileSystem
 foreach ($drive in $drives) {
   $driveRoot = $drive.Root
 
-  Get-ChildItem -Path $driveRoot *.tmp -Recurse | ForEach-Object { Remove-Item -Path $_.FullName }
-  Get-ChildItem -Path $driveRoot *._mp -Recurse | ForEach-Object { Remove-Item -Path $_.FullName }
-  Get-ChildItem -Path $driveRoot *.log -Recurse | ForEach-Object { Remove-Item -Path $_.FullName }
-  Get-ChildItem -Path $driveRoot *.gid -Recurse | ForEach-Object { Remove-Item -Path $_.FullName }
-  Get-ChildItem -Path $driveRoot *.chk -Recurse | ForEach-Object { Remove-Item -Path $_.FullName }
-  Get-ChildItem -Path $driveRoot *.old -Recurse | ForEach-Object { Remove-Item -Path $_.FullName }
+  Get-ChildItem -Path $driveRoot -Include ('*.tmp','*._mp','*.log','*.gid','*.chk','*.old') -Recurse -File | ForEach-Object { Remove-Item -Path $_.FullName -Force }
 
   $FoldersList = Get-ChildItem -Path $driveRoot -Directory -Recurse
   foreach ($Folder in $FoldersList) {
-    if (-not (gci -Force $Folder.FullName)) {
+    if (-not (Get-ChildItem -Force $Folder.FullName)) {
       Remove-Item -Path $Folder.FullName  -Force
     }
   } 
